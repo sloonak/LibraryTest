ad963a59122b52b976c17ee0339e73004407b67c
Akinok <nuqpa1@mail.ru>
2021-06-01 17:24:12 +0300

    Добавьте файлы проекта.

---
+++ LibraryTest/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTest
+{
+    public class Device
+    {
+        public void PowerOn() { }
+        public void PowerOff() { }
+        public void VolumeOn() { }
+        public void VolumeOff() { }
+        public void Mute() { }
+    }
+    public class Radio : Device
+    {
+        public void ChangeWave() { }
+    }
+    public class TV : Device
+    {
+        public void ChangeChannel() { }
+        public void ChangeInput() { }
+    }
+    public class Mobile : Device
+    {
+        public void Call() { }
+        public void ShowCharge() { }
+        public void ShowInfo() { }
+        public void Photo() { }
+        public void FlashOn() { }
+        public void FlashOff() { }
+    }
+}
